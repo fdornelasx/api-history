3a8fdebb4b447cf0ab473d956a0ef96a0294cd9f
Fillipe Dornelas <dornelasfillipe@gmail.com>
2017-06-05 12:42:26 -0300

    new api registry method

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='registry_utils',
+      version='0.0.1',
+      description='Python Registry API utilitary code',
+      author='Cognitivo ai / Dornelas',
+      author_email='dornelasfillipe@gmail.com',
+      install_requires=['requests','pytz'],
+      packages=['registry_utils'])
